7cf59d65c517e49a78ee29d6b1a68c95c1e2913f
Anson <chow.anson25@gmail.com>
2022-10-26 17:16:30 -0400

    Todo List

---
+++ Todo List Starter/app.js
@@ -0,0 +1,48 @@
+const list = document.querySelector(".list");
+const input = document.querySelector('input')
+
+let todoList = [];
+let todoInputValue = "";
+let counter = 0;
+
+function onInputChange(event) {
+  todoInputValue = event.target.value;
+}
+
+function addTodo() {
+  if (!todoInputValue) {
+    return;
+  }
+  const task = {
+    id: counter++,
+    task: todoInputValue,
+  };
+  todoList.push(task);
+  renderTodos();
+  input.value = "";
+  todoInputValue = "";
+}
+  
+
+function deleteTodo(id) {
+  todoList = todoList.filter((todo) => {
+    return todo.id !== id;
+  });
+  renderTodos();
+}
+
+function renderTodos() {
+  list.innerHTML = todoList
+    .map(
+      (elem) =>
+        `<li>
+           ${elem.task}
+              <button class="todo__delete" onclick="deleteTodo(${elem.id})">
+                  x
+              </button>
+          </li>`
+    )
+    .join("");
+}
+
+renderTodos();
